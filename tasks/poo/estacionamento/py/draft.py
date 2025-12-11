d3011e4c238a9b942ee95e5b0964c3edde932c04
yguito <yguqueiroz@alu.ufc.br>
2025-12-11 16:09:17 +0000

    bulindo

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+
 class Veiculo(ABC):
     def __init__(self, id: str, entrada: int, tipo: str):
         self.id = id
@@ -7,6 +8,9 @@ class Veiculo(ABC):
     @abstractmethod
     def calcular_valor(self, saida: int) -> float:
         pass
- 
 
+    def __str__(self):
+        fmt_tipo = self.tipo.rjust(10, "-")
+        fmt_id = self.id.ljust(10, "-")
+        return f"{fmt_tipo} | {fmt_id} : {self.entrada}"
 
