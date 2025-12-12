0e5a8a80284591efb2e50f43bb54ead7da40a49c
yguito <yguqueiroz@alu.ufc.br>
2025-12-12 20:21:03 +0000

    continunando atividade

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -14,3 +14,17 @@ class Veiculo(ABC):
         fmt_id = self.id.ljust(10, "-")
         return f"{fmt_tipo} | {fmt_id} : {self.entrada}"
 
+
+class Bike(Veiculo):
+    def __init__(self, id: str, entrada: int):
+        super().__init__(id, entrada, "Bike", entrada)
+
+    def calcular_valor(self, saida_saida: int) -> float:
+        return 3.00
+class Moto(Veiculo):
+    def __init__(self, id: str, entrada: int):
+        super().__init__(id, "Moto", entrada)
+
+    def calcular_valor(self, hora_saida: int) -> float:
+        tempo = hora_saida - self.entrada
+        return tempo / 20.0
